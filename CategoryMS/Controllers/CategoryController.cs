79549a37ee074e20dcb72224048f578e717f0278
Plínio Tirado <phtirado@gmail.com>
2019-06-14 13:25:29 -0300

    Change the name used to create a category

---
+++ CategoryMS/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CategoryMS.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CategoryMS.Controllers
+{
+    [Route("api/Category")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly CategoryContext _context;
+
+        public CategoryController(CategoryContext context)
+        {
+            _context = context;
+
+            if(_context.GetCategories.Count() == 0)
+            {
+                _context.GetCategories.Add(new Category { Name = "Beer 2" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Category
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            return await _context.GetCategories.ToListAsync();
+        }
+
+        // GET api/Category/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(long id)
+        {
+            var category = await _context.GetCategories.FindAsync(id);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // POST api/Category
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+            _context.GetCategories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCategories), new { Id = category.Id }, category);
+        }
+
+        /*// PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
---
+++ CategoryMS/Models/CategoryContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CategoryMS.Models
+{
+    public class CategoryContext: DbContext
+    {
+        public CategoryContext(DbContextOptions<CategoryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> GetCategories { get; set; }
+    }
+}
